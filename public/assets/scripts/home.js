4e5968c5dcb4cd95fff716016c355ff68758897a
Pedro Henrique <phfs28@gmail.com>
2021-04-02 20:35:25 -0300

    Adicionado validação para e-mail existente no cadastro de newsletter

--- public/assets/scripts/home.js
+++ public/assets/scripts/home.js
@@ -9,24 +9,41 @@ if (page) {
     const setNewsletterFormListener = (newsletter) => {
         const form = newsletter.querySelector('form');
 
-        form.addEventListener('submit', (event) => {
+        form.addEventListener('submit', async (event) => {
             event.preventDefault();
 
-            const data = getFormValues(form);
+            const { email } = getFormValues(form);
 
             const submitButton = form.querySelector('button[type="submit"]');
             const submitButtonText = submitButton.innerHTML;
+
             toggleButtonLoader(submitButton);
 
-            database.collection('newsletter').add({ email: data.email })
-            .then(() => {
+            try {
+                const response = await database.collection('newsletter').where('email', '==', email).get();
+                const data = [];
+
+                response.forEach(item => data.push(item.data()) );
+
+                if (data.length > 0) {
+                    throw new Error('O e-mail informado já está cadastrado.')
+                } 
+
+                await database.collection('newsletter').add({ email });
+
+                toggleButtonLoader(submitButton, submitButtonText);
+
                 createAlert('Seu e-mail foi cadastrado com sucesso!', 'success');
-                resetFormValues(form);
-            })
-            .catch(() => {
-                createAlert('Não foi possível cadastrar seu e-mail. Por favor, tente novamente.', 'success');
-            })
-            .finally(() => toggleButtonLoader(submitButton, submitButtonText));
+
+                resetFormValues(form);                
+
+            } catch (error) {
+                const defaultMessage = 'Não foi possível cadastrar o e-mail. Por favor, tente novamente.';
+
+                createAlert(error.message || defaultMessage, 'danger');
+
+                toggleButtonLoader(submitButton, submitButtonText);
+            }
         });
     }
 
