af35547090e422441b99c6e105ebd26c325aad5a
Pedro Henrique <phfs28@gmail.com>
2021-04-10 18:36:38 -0300

    Finalização da página portifólio

--- public/assets/scripts/portifolio.js
+++ public/assets/scripts/portifolio.js
@@ -3,6 +3,44 @@ import { fetchCollection } from './utils';
 const page = document.querySelector('#portifolio');
 
 if (page) {
+    const displayCarroussel = (item) => {
+        const carroussel = page.querySelector('.carroussel-wrapper');
+        carroussel.classList.add('open');
+
+        const slider = carroussel.querySelector('.swiper-container');
+        const sliderWrapper = carroussel.querySelector('.swiper-wrapper');
+
+        carroussel.querySelector('.name').innerHTML = item.name;
+
+        sliderWrapper.innerHTML = '';
+
+        item.photos.forEach(photo => {
+            let template = document.createElement('div');
+            template.classList.add('swiper-slide');
+
+            template.innerHTML = `<img src="${photo}" alt="Produto">`;
+
+            sliderWrapper.append(template);
+        });
+
+        const sliderInstance = new Swiper(slider, {
+            slidesPerView: 1,
+            lazy: true,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true
+            },
+            keyboard: {
+                enabled: true,
+                onlyInViewport: false,
+            }
+        });
+
+        carroussel.querySelector('.button-close').addEventListener('click', () => {
+            carroussel.classList.remove('open');
+            sliderInstance.destroy();
+        });
+    }
 
     const renderPortifolio = async (page) => {
         const portifolio = await fetchCollection('portifolio');
@@ -24,6 +62,12 @@ if (page) {
             <img src="${item.mainPhoto}" alt="Casamentos">
             <span>${item.name}</span>
         `;
+
+        item.photos.unshift(item.mainPhoto);
+
+        template.addEventListener('click', event => {
+            displayCarroussel(item);
+        });
     
         return template;
     }
