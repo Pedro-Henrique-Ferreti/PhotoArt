d77b7ba74e7d50a4103b625e62f3317f47d04afc
Pedro Henrique <phfs28@gmail.com>
2021-04-15 21:01:04 -0300

    Added edit user name

--- public/assets/scripts/account.js
+++ public/assets/scripts/account.js
@@ -35,6 +35,14 @@ if (page) {
         });
     }
 
+    const validateUserDataForm = (name) => {
+        if (name.trim() === '') {
+            createAlert('Preencha o nome', 'danger');
+            return false;
+        }
+        return true;
+    }
+
     const validateChangePasswordForm = (password, passwordRepeat) => {
         if (password.trim() === '') {
             createAlert('Preencha a nova senha', 'danger');
@@ -55,6 +63,33 @@ if (page) {
         return true;
     }
 
+    const submitUserDataForm = async (form) => {
+        const submitButton = form.querySelector('button[type="submit"]');
+        const submitButtonText = submitButton.innerHTML;
+        const { name } = getFormValues(form);
+
+        toggleButtonLoader(submitButton);
+
+        if (!validateUserDataForm(name)) {
+            toggleButtonLoader(submitButton, submitButtonText);
+            return;
+        }
+
+        try {
+            await auth.currentUser.updateProfile({ 
+                displayName: name
+            });
+            createAlert('Alterações salvas com sucesso', 'success');
+            toggleButtonLoader(submitButton, submitButtonText);
+            
+            renderUserData();
+
+        } catch (error) {
+            createAlert(translateError(error.code), 'danger');
+            toggleButtonLoader(submitButton, submitButtonText);
+        }
+    }
+
     const submitChangePasswordForm = async (form) => {
         const submitButton = form.querySelector('button[type="submit"]');
         const submitButtonText = submitButton.innerHTML;
@@ -70,12 +105,28 @@ if (page) {
         try {
             await auth.currentUser.updatePassword(password);
             createAlert('Sua senha foi alterada com succeso', 'success');
-            toggleButtonLoader(submitButton, submitButtonText);
+            toggleButtonLoader(submitButton, submitButtonText);           
 
         } catch (error) {
             createAlert(translateError(error.code), 'danger');
             toggleButtonLoader(submitButton, submitButtonText);
         }
+    }
+
+    const renderUserData = async () => {
+        const user = await loadCurrentUser();
+
+        const userDataForm = page.querySelector('#form-user-data');
+        const header = page.querySelector('.header');
+        const picture = header.querySelector('.picture');
+        const name = header.querySelector('.name');
+        const email = header.querySelector('.email');
+
+        picture.innerHTML = user.displayName.split('')[0];
+        name.innerHTML = user.displayName;
+        email.innerHTML = user.email;
+
+        userDataForm.querySelector('input[name="name"]').value = user.displayName;
     }
 
     const init = async () => {
@@ -84,9 +135,17 @@ if (page) {
         if (!user) {
             window.location.href = '/auth.html';
         } else {
-            togglePage();
-            
+
+            const userDataForm = page.querySelector('#form-user-data');
             const passwordForm = page.querySelector('#form-alter-password');
+
+            togglePage();
+            renderUserData(user, userDataForm);
+
+            userDataForm.addEventListener('submit', event => {
+                event.preventDefault();
+                submitUserDataForm(userDataForm);
+            });
 
             passwordForm.addEventListener('submit', event => {
                 event.preventDefault();
